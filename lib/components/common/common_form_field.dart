8850a1aa30324f95fab0465b8c001d84dcba26dd
Yunho <potatohead12@naver.com>
2021-08-03 23:45:32 +0900

    에어비엔비

---
+++ lib/components/common/common_form_field.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/styles.dart';
+
+class CommonFormField extends StatelessWidget {
+  final prefixText;
+  final hintText;
+
+  const CommonFormField({required this.prefixText, required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        TextFormField(
+          textAlignVertical: TextAlignVertical.bottom, // 글자를 내려줌.
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(
+                top: 30, left: 20, bottom: 10), // 글자가 대려갈 공간을 만들어줌.
+            hintText: hintText,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(
+                color: Colors.black,
+                width: 2,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 8,
+          left: 20,
+          child: Text(
+            prefixText,
+            style: overLine(),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/components/home/home_body_popular.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeBodyPopular extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildPopularTitle(),
+        _buildPopularList(),
+      ],
+    );
+  }
+
+  Widget _buildPopularTitle() {
+    return SizedBox();
+  }
+
+  Widget _buildPopularList() {
+    //homebodypopularitem넣을 자리
+    return SizedBox();
+  }
+}
